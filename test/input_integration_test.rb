e2464645bad412d269ae66c74232cbd21477fe5a
Darren Smith <darrenks@golfscript.com>
2021-06-23 22:56:26 -0600

    input integration test

---
+++ test/input_integration_test.rb
@@ -0,0 +1,13 @@
+# test nibbles on a program which computes all primes < 400 then prints 62nd
+test_program = "@+@"
+test_file = 'integration_test'
+testnbl = test_file+'.nbl'
+testnbb = test_file+'.nbb'
+`rm #{testnbl} #{testnbb} out.hs 2> /dev/null`
+`ghc nibbles.hs 2> /dev/null`
+File.open(testnbl,'w'){|f|f<<test_program}
+
+out = `echo 1 2 3 | nibbles #{testnbl} 2> /dev/null`
+out == "1\n2\n3\n6\n" or raise 'generated haskell produces wrong output %p' % out
+
+puts 'pass input test'
